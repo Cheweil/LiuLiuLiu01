d002accbcf434d27fc85c31e7005ee8be16ec212
Che Wei <victor@Victorteki-MacBook-Pro-2.local>
2015-06-29 17:24:01 -0700

    Initial Commit

---
+++ CheWei'sAssignment01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CheWei'sAssignment01
+//
+//  Created by Che Wei on 6/29/15.
+//  Copyright (c) 2015 Foothill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
